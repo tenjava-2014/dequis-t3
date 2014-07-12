d93bdedb215a055c9b85b9fc4da43604151931dd
dequis <dx@dxzone.com.ar>
2014-07-12 13:52:02 -0300

    to be honest, i have no idea. but this is it

--- src/main/java/com/tenjava/entries/dequis/t3/TenJava.java
+++ src/main/java/com/tenjava/entries/dequis/t3/TenJava.java
@@ -1,5 +1,6 @@
 package com.tenjava.entries.dequis.t3;
 
+import java.lang.Math;
 import java.util.List;
 import java.util.Random;
 import java.util.Arrays;
@@ -9,10 +10,16 @@ import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.Chunk;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.Sign;
+
 
 public class TenJava extends JavaPlugin {
     public void onEnable() {
-
+        /* there is nothing to do */
     }
 
     @Override
@@ -25,8 +32,8 @@ public class TenJava extends JavaPlugin {
             byte[] result = new byte[32768];
             for (int x = 0; x < 16; x++) {
                 for (int z = 0; z < 16; z++) {
-                    for (int y = 0; y < 10; y++) {
-                        result[(x * 16 + z) * 128 + y] = (byte)Material.STONE.getId();
+                    for (int y = 0; y < 64; y++) {
+                        result[(x * 16 + z) * 128 + y] = (byte)Material.OBSIDIAN.getId();
                     }
 
                 }
@@ -36,7 +43,7 @@ public class TenJava extends JavaPlugin {
 
         @Override
         public List<BlockPopulator> getDefaultPopulators(World world) {
-            return Arrays.asList();
+            return Arrays.asList((BlockPopulator) new FlagPopulator());
         }
 
         @Override
@@ -45,6 +52,48 @@ public class TenJava extends JavaPlugin {
             int z = 0;
             int y = 127;
             return new Location(world, x, y, z);
+        }
+    }
+
+
+    class FlagPopulator extends BlockPopulator {
+        private double logRand(int max, Random random) {
+            return max - Math.log10(random.nextInt((int) Math.pow(10, max)));
+        }
+
+        public void populate(World world, Random random, Chunk source) {
+            Block block = null;
+            if (random.nextInt(100) <= 90) {
+                int centerX = (source.getX() << 4) + random.nextInt(16);
+                int centerZ = (source.getZ() << 4) + random.nextInt(16);
+                int centerY = world.getHighestBlockYAt(centerX, centerZ) + 1;
+
+                int height = random.nextInt(20);
+                for (int y = centerY; y < centerY + height; y++) {
+                    block = world.getBlockAt(centerX, y, centerZ);
+                    block.setType(Material.FENCE);
+                }
+
+            }
+            int blocksPerChunk = (int) (logRand(5, random) * 2);
+
+            for (int i = 0; i < blocksPerChunk; i++) {
+                int centerX = (source.getX() << 4) + random.nextInt(16);
+                int centerZ = (source.getZ() << 4) + random.nextInt(16);
+                int y = random.nextInt(64) + 64;
+                block = world.getBlockAt(centerX, y, centerZ);
+                Material mat = Material.STONE;
+                int rand = (int) Math.floor(Math.max(logRand(10, random) * 1.5, 10));
+                switch (rand) {
+                    case 0: mat = Material.STONE; break;
+                    case 1: mat = Material.WOOD; break;
+                    case 2: mat = Material.LOG; break;
+                    case 3: mat = Material.COAL_ORE; break;
+                    case 4: mat = Material.IRON_ORE; break;
+                    case 5: mat = Material.DIAMOND_ORE; break;
+                }
+                block.setType(mat);
+            }
         }
     }
 }
